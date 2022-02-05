690a2617b2e66cf0531222a3f9c69fa1f3f18133
Kush Patel <kushpatel2577@gmail.com>
2022-02-05 15:54:36 -0500

    Climber class made
    Pnuematic and motor initialization

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,36 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+<<<<<<< Updated upstream
 public final class Constants {}
+=======
+public final class Constants {
+
+    // Drivetrain motor IDs
+    public static final int LEFT_PRIMARY_MOTOR_ID               = 0;
+    public static final int LEFT_REPLICA_1_MOTOR_ID             = 0;
+    public static final int LEFT_REPLICA_2_MOTOR_ID             = 0;
+    public static final int RIGHT_PRIMARY_MOTOR_ID              = 0;
+    public static final int RIGHT_REPLICA_1_MOTOR_ID            = 0;
+    public static final int RIGHT_REPLICA_2_MOTOR_ID            = 0;
+
+    //Drivetrain pnuematic channels
+    public static final int HIGH_GEAR_PNUEMATIC_ID              = 0;
+    public static final int LOW_GEAR_PNUEMATIC_ID               = 0;
+    //Intake motor ID
+    public static final int INTAKE_MOTOR_ID                     = 0;
+
+    //Intake pneumatic channels
+    public static final int INTAKE_IN_PNUEMATIC_ID              = 0;
+    public static final int INTAKE_OUT_PNUEMATIC_ID             = 0;
+
+    //Climber Motor ID
+    public static final int CLIMBER_MOTOR_ID                    = 0;
+
+    //Climber Pneumatic Channels
+    public static final int CLIMBER_PNEUMATIC_1_IN              = 0;
+    public static final int CLIMBER_PNUEMATIC_1_OUT             = 0;
+    public static final int CLIMBER_PNUEMATIC_2_IN              = 0; 
+    public static final int CLIMBER_PNEUMATIC_2_OUT             = 0;
+}
+>>>>>>> Stashed changes
---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import frc.robot.Constants;
+
+public class Climber {
+    // Initialize Variables
+    CANSparkMax climberControl;
+    DoubleSolenoid climberPneumatic1;
+    DoubleSolenoid climberPneumatic2;
+    
+
+    public Climber() {
+        climberControl = new CANSparkMax(Constants.CLIMBER_MOTOR_ID, MotorType.kBrushless);
+        climberPneumatic1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.CLIMBER_PNUEMATIC_1_OUT, Constants.CLIMBER_PNEUMATIC_1_IN);
+        climberPneumatic2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.CLIMBER_PNEUMATIC_2_OUT, Constants.CLIMBER_PNUEMATIC_2_IN);
+    }
+
+    public void climberPneumaticOff() {
+        climberPneumatic1.set(Value.kOff);
+        climberPneumatic2.set(Value.kOff);
+    }
+
+    public void climberPneumaticExtend() {
+        climberPneumatic1.set(Value.kForward);
+        climberPneumatic2.set(Value.kForward);
+    }
+    
+    public void climberPneumaticRetract() {
+        climberPneumatic1.set(Value.kReverse);
+        climberPneumatic2.set(Value.kReverse);
+    }
+}
