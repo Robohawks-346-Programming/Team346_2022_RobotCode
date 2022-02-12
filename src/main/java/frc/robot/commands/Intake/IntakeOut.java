c7e7879eb390fe2e9c9b50fb7f9961522b66e760
kushpatel2577 <kushpatel2577@gmail.com>
2022-02-11 21:49:16 -0500

    Commands for shooter, drivetrain, and intake
    Commands for shooter, drivetrain, and intake were made. Initialized PS4controller. Need to initialize button board.

--- src/main/java/frc/robot/commands/Intake/IntakeOut.java
+++ src/main/java/frc/robot/commands/Intake/IntakeOut.java
@@ -2,16 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-<<<<<<< Updated upstream
-package frc.robot.commands.Drivetrain;
-
-import frc.robot.Robot;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-
-/** An example command that uses an example subsystem. */
-public class JoystickDrive extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-=======
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
@@ -21,24 +11,16 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class ExampleCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final ExampleSubsystem m_subsystem;
->>>>>>> Stashed changes
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-<<<<<<< Updated upstream
-  public JoystickDrive() {
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(Robot.drivetrain);
-    
-=======
   public ExampleCommand(ExampleSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
->>>>>>> Stashed changes
   }
 
   // Called when the command is initially scheduled.
